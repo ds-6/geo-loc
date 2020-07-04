73d3ced77340f5725f846d142e5068122ebab297
deepak <deepaksoni06@gmail.com>
2020-07-04 19:27:27 +0530

    set apikey in heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express ();
 
-const dbURI = process.env.API_KEY;
+const dbURI = 'mongodb://'+process.env.API_KEY+'@project-1-shard-00-00-ea9wt.mongodb.net:27017,project-1-shard-00-01-ea9wt.mongodb.net:27017,project-1-shard-00-02-ea9wt.mongodb.net:27017/project1?ssl=true&replicaSet=project-1-shard-0&authSource=admin&retryWrites=true&w=majority'
 mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(result=>{
     
