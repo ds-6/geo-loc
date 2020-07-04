aa98a8a8b3e02f017a9f28169b37603194b64741
deepak <deepaksoni06@gmail.com>
2020-07-04 19:08:58 +0530

    avoided env file

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const Userloc = require('./models/userloc');
 require('dotenv').config();
 
 const app = express ();
-console.log(process.env);
 
-const dbURI = process.env.dbKey;
+const dbURI = "mongodb://deepak:12345@project-1-shard-00-00-ea9wt.mongodb.net:27017,project-1-shard-00-01-ea9wt.mongodb.net:27017,project-1-shard-00-02-ea9wt.mongodb.net:27017/project1?ssl=true&replicaSet=project-1-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(result=>{
     
